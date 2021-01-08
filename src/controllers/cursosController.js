00f5fe4b675754f9558e282f9031cc8323dd6b31
Adrian Cano <adriancano@MacBook-Air-de-Adrian.local>
2021-01-08 19:57:14 -0300

    crud

--- src/controllers/cursosController.js
+++ src/controllers/cursosController.js
@@ -2,17 +2,39 @@ const cursosCtrl = {};
 
 const Curso = require("../models/cursosModel");
 
-cursosCtrl.altaCurso = async(req, res) => {
-const{nombre, duracion, precio} = req.body;
+cursosCtrl.altaCurso = async (req, res) => {
+  const { nombre, duracion, precio } = req.body;
 
-const nuevoCurso = new Curso({nombre, duracion, precio});
-await nuevoCurso.save();
+  const nuevoCurso = new Curso({ nombre, duracion, precio });
+  await nuevoCurso.save();
 
-res.json(
-    nuevoCurso
-)
-
-
+  res.json(nuevoCurso);
 }
 
+cursosCtrl.updateCurso = async(req, res) =>{
+    const { nombre, duracion, precio } = req.body;
+     await Curso.findByIdAndUpdate(req.params.id, {nombre, duracion, precio});
+
+    res.json("Actualizado correctamente");
+}
+
+cursosCtrl.traerCursos = async (req, res)=>{
+    let cursos = await Curso.find({});
+
+    res.json(cursos);
+}
+
+
+cursosCtrl.deleteCurso = async(req, res) =>{
+ 
+     await Curso.findByIdAndDelete(req.params.id);
+
+    res.json("borrado correctamente");
+}
+
+cursosCtrl.traerCurso = async(req, res) =>{
+    let curso = await Curso.find({_id: req.params.id});
+
+    res.json(curso);
+}
 module.exports = cursosCtrl;
--- src/routes/cursosRoutes.js
+++ src/routes/cursosRoutes.js
@@ -3,10 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 
-const{ altaCurso } = require("../controllers/cursosController");
+const{ altaCurso, updateCurso, traerCursos, deleteCurso, traerCurso} = require("../controllers/cursosController");
  
 
 router.post("/curso/nuevo", altaCurso);
+router.put("/curso/update/:id", updateCurso);
+router.get("/curso", traerCursos);
+router.delete("/curso/borrar/:id", deleteCurso);
+router.get("/curso/:id", traerCurso);
  
 
 module.exports = router;
